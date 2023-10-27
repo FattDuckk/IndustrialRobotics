eab132bbc017d0c5da1e17bc37f4877bae516aa1
FattDuckk <45501903+FattDuckk@users.noreply.github.com>
2023-10-27 14:50:33 +1100

    d

--- testing.m
+++ testing.m
@@ -19,4 +19,40 @@ r=UR3
 axis equal
   r.model.teach()
 % robot.TestMoveJoints
+
+
 a=T(r.model.fkine(r.model.getpos()))
+
+%%
+robot=KUKA
+r=UR3
+qrKUKA = [0	pi/3   0	    5*pi/12	    0	    pi/4   pi 0 0];
+robot.model.teach(qrKUKA)
+
+
+%%
+
+qrKUKA = [0	pi/3   0	    pi/4	    0	    5*pi/12   pi 0 0];
+        qrUR3 = [0	-3*pi/4   -pi/4	-pi/2           pi/2	-pi/2   0 0];
+
+height=0.7
+%%
+
+robot=KUKA
+qrKUKA = [0	pi/3   0	    pi/4	    0	    5*pi/12   pi 0 0];
+pos=robot.model.fkine(qrKUKA)
+
+r=UR3
+qrUR3 = [0	-3*pi/4   -pi/4	-pi/2           pi/2	-pi/2   0 0];
+pos2=r.model.fkine(qrUR3)
+
+height=0.7
+coordinate=[-0.06  0.06    height
+         -0.06  0       height
+         -0.06  -0.6    height
+            0   0.06    height
+            0   0       height
+            0   -0.6    height
+          0.06 0.06     height
+          0.06 0        height
+          0.06 -0.6     height]
